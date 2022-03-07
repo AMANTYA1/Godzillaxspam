4b49f27b99fdb616ac264f0905d6f3c7f6d9d170
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-07 20:56:26 +0530

    Update raid.py

--- KIGO/plugins/raid.py
+++ KIGO/plugins/raid.py
@@ -7,8 +7,8 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from resources.data import RAID, REPLYRAID, DEADLYSPAM
-from Godfather import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
-from Godfather import CMD_HNDLR as hl
+from KIGO import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
+from KIGO import CMD_HNDLR as hl
 
 
 que = {}
